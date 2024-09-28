50059b884a2cd3f5b881b6afc82a338d53088f27
fabiosantos-info <32442564+fabiosantos-info@users.noreply.github.com>
2024-09-28 14:05:40 +0000

    terraform ec2

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,19 @@
-module "ec2"{
-    source = "./modules/ec2-aws"
-    region = "us-east-2"
-
+module "ec2_instance" {
+  source        = "./ec2_module"
+  region        = var.region
+  ami           = var.ami
+  instance_name = var.instance_name
 }
 
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "ami" {
+  default = "ami-0c55b159cbfafe01e" # Exemplo de AMI, ajuste conforme necessário
+}
+
+variable "instance_name" {
+  default = "MyFirstInstance"
+}
 
